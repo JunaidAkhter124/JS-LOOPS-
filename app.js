6e9e15d3ee6d4d2bc94b3558eaa70a2a0eda1a59
junaid akhter <junaidakhter945@gmail.com>
2024-09-23 21:15:57 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+// loops
+// var userinput =prompt("Enter your city");
+// var citiesArr =["karachi","lahore","multan","quetta"];
+
+// if (userinput === citiesArr[0]){
+//     console.log("value match")
+// }
+
+//  else if (userinput === citiesArr[1]){
+//     console.log("value match") 
+// }
+// else if (userinput === citiesArr[2]){
+//     console.log("value match") 
+// }
+// else if (userinput === citiesArr[3]){
+//     console.log("value match") 
+// }
+
+// else{
+//     console.log("value not found..");
+// }
+
+
+// var flag = false;
+
+
+// for (var i=0; i <=5; i++){
+//     if(userinput === citiesArr[i]){
+//         console.log("city found");
+//     }
+// }
+
+// if (flag === false){
+//     console.log("hello world")
+// }
+
+//  var userinput =prompt("Enter your city");
+//  userInput = userInput.toUppercase();
+//  var citiesArr =["Karachi","Lahore","Multan","Quetta"];
+
+
